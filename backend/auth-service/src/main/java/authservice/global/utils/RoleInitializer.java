b014f12844f8e2d4106683207f198148b0e811f5
kmg97 <minku4820@gmail.com>
2026-07-29 20:37:46 +0900

    fix: set run order of ApplicationRunner
    - FIx error where User is created before Role
    - Add @Order(1) to RoleInitializer to create Role first
    - Add @Order(2) to UserInitializer

--- backend/auth-service/src/main/java/authservice/global/utils/RoleInitializer.java
+++ backend/auth-service/src/main/java/authservice/global/utils/RoleInitializer.java
@@ -6,8 +6,10 @@ import authservice.role.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@Order(1)
 @Component
 @RequiredArgsConstructor
 public class RoleInitializer implements ApplicationRunner {
--- backend/auth-service/src/main/java/authservice/global/utils/UserInitializer.java
+++ backend/auth-service/src/main/java/authservice/global/utils/UserInitializer.java
@@ -7,9 +7,11 @@ import authservice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@Order(2)
 @Component
 @RequiredArgsConstructor
 public class UserInitializer implements ApplicationRunner {
